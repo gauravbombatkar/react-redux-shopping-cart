2012e44a62e11f6d9576d0e44c2ba786cbd318ac
gauravbombatkar <gbombatkar@gmail.com>
2020-07-22 00:33:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import React from 'react';
 function App() {
   return (
     <div className="grid-container">
-
+    <header>
+    <a href="/">React Shopping Cart</a>
+    </header>
+    <main>
+    Product List
+    </main>
+    <footer>
+    All Right is reserved.
+    </footer>
     </div>
   );
 }
